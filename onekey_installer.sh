9b9d7c9e61272be8406d59a4783e411f7e8c9976
Eric <eric@Erics-MacBook-Pro.local>
2019-06-01 13:23:12 -0500

    Added onekeyinstaller

---
+++ onekey_installer.sh
@@ -0,0 +1,64 @@
+function networkWarning()
+{
+    echo "ERROR: Fail to download GitHub Files, please check your Internet connection!"
+    sleep 3
+    exit
+}
+
+function downloadFiles()
+{
+    cd
+    cd Downloads
+    echo "------------------------------------------------------"
+    echo "--| Downloading GitHub files into Downloads folder |--"
+    echo "------------------------------------------------------"
+    curl --silent -L -O "https://github.com/gnodipac886/MatebookXPro-hackintosh/archive/6.1.zip" || networkWarn
+    echo "Download Complete"
+    sleep 1
+    echo "Unzipping Files"
+    unzip -qu "6.1"
+    echo "Files unzipped"
+    echo "Now running autoinstallation script..."
+    sleep 1
+}
+
+function runScript()
+{
+    echo "----------------------"
+    echo "**** Install Now? ****"
+    echo "----------------------"
+    read -p "Type y/n : " lfm_selection
+    case "${lfm_selection}" in
+    y)
+    cd
+    cd Downloads/MatebookXPro-hackintosh-6.1
+    ./autoinstaller.command
+    ;;
+
+    n)
+    cd
+    cd /Downloads/MatebookXPro-hackintosh-6.1
+    echo "Please help a broke student out please :)"
+    echo "Thank you! Have a great day!"
+    sleep 3
+    open .//Help\ a\ Broke\ Student\ out/paypal.png
+    open .//Help\ a\ Broke\ Student\ out/venmo.jpg
+    exit
+    ;;
+
+    *)
+    echo "Please press y or n lol"
+    echo "Exiting"
+    sleep 1
+    exit
+    esac
+}
+function main()
+{
+    downloadFiles
+    sleep 3
+    runScript
+    sleep 3
+}
+
+main
